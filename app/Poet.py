496d7f3ade4ef3c5a2e3511244f1f2a2955b028b
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-03-10 15:55:14 -0400

    feat: Update poems generation logic

--- app/Poet.py
+++ app/Poet.py
@@ -13,15 +13,48 @@ class Poet(Base):
         self.rulesFiles = config["Poet"]["PoetRulesFile"]
         blob = open(self.rulesFiles).read()
         self.rules = json.loads(blob)
+        self.getAllPossibleCombinations(self.rules)
         self.log("\n* Poet intialized")
         self.log("Rules file\t{}".format(self.rulesFiles))
+
+    def getAllPossibleCombinations(self, rules):
+        silos = rules.keys()
+        combinations = []
+        for silo1 in silos:
+            for silo2 in silos:
+                if silo1 != silo2:
+                    combinations.append("{},{}".format(silo1, silo2))
+        random.shuffle(combinations)
+        return combinations
+
+    def storeCombinations(self, combinations):
+        combinations = filter(lambda c: c != "", combinations)
+        file = open("combinations.txt", "w+")
+        for combination in combinations:
+            file.write("{};".format(combination))
+        file.close()
+        pass
+
+    def pickOneCombination(self):
+        try:
+            file = open("combinations.txt", "r")
+            contents = file.read()
+            combinations = contents.split(";")
+            combinations = list(filter(lambda c: c != "", combinations))
+            if len(combinations) == 0:
+                combinations = self.getAllPossibleCombinations(self.rules)
+        except Exception as e:
+            combinations = self.getAllPossibleCombinations(self.rules)
+        index = random.randrange(len(combinations)) - 1
+        combination = combinations.pop(index)
+        self.storeCombinations(combinations)
+
+        return combination
 
     def makePoem(self):
         grammar = tracery.Grammar(self.rules)
         grammar.add_modifiers(base_english)
-        # Build origin
-        keys = [*self.rules.keys()]
-        random.shuffle(keys)
-        keys = keys[:2]
-        origin = "#{}#\n#{}#".format(*keys)
+        combination = self.pickOneCombination()
+        combination = combination.split(",")
+        origin = "#{}#\n#{}#".format(*combination)
         return grammar.flatten(origin)
